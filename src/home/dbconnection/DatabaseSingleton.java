3a89fa4099b233e0aadf1c822dd5642ebd434b2d
gebrekirstos <gebrekirstos2015@gmail.com>
2017-04-19 11:18:48 -0500

    [I add connection module]

---
+++ src/home/dbconnection/DatabaseSingleton.java
@@ -0,0 +1,16 @@
+package home.dbconnection;
+
+/**
+ * Created by gebre on 4/18/2017.
+ */
+public class DatabaseSingleton {
+    private static Database database = null;
+    public static Database getDatabase(){
+        if(database == null){
+            database = new Database();
+        }
+
+        return database;
+    }
+
+}
