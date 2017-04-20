39d75b51a8f6cd3a439a735dddb3eb0888bf4898
Dora Khiev <khievdora@gmail.com>
2017-04-20 10:35:26 -0500

    [REFACTOR]: Change parameter type of the method.

--- src/main/authenticationsub/AuthenticationSubcriber.java
+++ src/main/authenticationsub/AuthenticationSubcriber.java
@@ -1,13 +1,13 @@
 package main.authenticationsub;
 
-import main.model.User;
+import main.model.Account;
 
 /**
  * Created by Dora on 4/19/2017.
  */
 public interface AuthenticationSubcriber {
 
-    public void onLoginSuccess(User user);
+    public void onLoginSuccess(Account account);
     public void onLoginFail(String errMessage);
     public void onSessionExpired();
 
