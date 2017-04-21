88bd62b4e1fb3c288b75540b2b50629fba7822f1
Dora Khiev <khievdora@gmail.com>
2017-04-21 14:06:53 -0500

    [IMPROVEMENT]: Improve the tab order in LoginWindow.

--- src/main/controller/LoginController.java
+++ src/main/controller/LoginController.java
@@ -1,11 +1,20 @@
 package main.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import main.LoginWindow;
 import main.authenticationsub.AuthenticationService;
 import main.authenticationsub.AuthenticationSubcriber;
@@ -13,16 +22,20 @@ import main.authenticationsub.AuthenticatoinFacade;
 import main.model.Account;
 
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Dora on 4/20/2017.
  */
-public class LoginController implements AuthenticationSubcriber {
+public class LoginController implements Initializable, AuthenticationSubcriber {
 
     @FXML
     private TextField txtUserName;
     @FXML
     private PasswordField txtPassword;
+    @FXML
+    private Button btnLogin;
     @FXML
     private Label lblMessage;
     @FXML
@@ -38,6 +51,35 @@ public class LoginController implements AuthenticationSubcriber {
     }
 
     @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Resource is loaded!!!");
+        txtUserName.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    lblMessage.setText("");
+                }
+            }
+        });
+        txtPassword.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    lblMessage.setText("");
+                }
+            }
+        });
+        txtPassword.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    onButtonLoginClicked();
+                }
+            }
+        });
+    }
+
+    @Override
     public void onLoginSuccess(Account account) {
         // Navigate to Welcome screen
         System.out.println("Login success!!!");
@@ -46,6 +88,7 @@ public class LoginController implements AuthenticationSubcriber {
 
     @Override
     public void onLoginFail(String errMessage) {
+        txtUserName.requestFocus();
         // Display error message
         lblMessage.setText(errMessage);
         clearText();
@@ -81,5 +124,4 @@ public class LoginController implements AuthenticationSubcriber {
         imgHotel.setImage(new Image(imageUrl));
         imgHotel.setSmooth(true);
     }
-
 }
