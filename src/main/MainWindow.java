4bbe6b073b8b38fb8c2a4fdabd7ba670875ce6ec
gebrekirstos <gebrekirstos2015@gmail.com>
2017-04-23 11:07:02 -0500

    {Modified Main}

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.Shared.UrlLoader;
 
 /**
  * Created by gebre on 4/19/2017.
@@ -13,9 +14,10 @@ public class MainWindow extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
-        Scene scene = new Scene(root, 400,400);
+        Parent root = FXMLLoader.load(getClass().getResource(UrlLoader.loadView("MainWindow.fxml")));
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    
 }
--- src/main/controller/RoomTabController.java
+++ src/main/controller/RoomTabController.java
@@ -1,11 +1,28 @@
 package main.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.awt.*;
 
 /**
  * Created by gebre on 4/23/2017.
  */
 public class RoomTabController {
+    @FXML
+    private TextField txtRoomSearch;
+    @FXML
+    private Button btnRoomSearch;
+
+    @FXML
+    private TableView tblVWRoom;
+
+
+
 
     public void onTxtRoomSearch(){
 
@@ -14,8 +31,15 @@ public class RoomTabController {
 
     }
     public void onBtnRoomAddClicked(){
-        FXMLLoader loader = new FXMLLoader();
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../resource/view/AddRoom.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
     }
     public void onBtnRoomRefreshClicked(){
